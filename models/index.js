f7bd7316d2ed6914f7f7ff16c4e1f069833c0b92
gigi.upNorth <leung.gerry@gmail.com>
2022-08-26 00:27:29 -0400

    [models] Add model definitions
    Descritpion:
     - Describe Category, Products, Tag, and ProductTag tables
     - Adding Product category relationship
     - add Product tag relationship

--- models/index.js
+++ models/index.js
@@ -1,16 +1,35 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+// Associate Products to tags, so that when we Query Tag, we should see the all the products that 
+// belong to that tag
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: "product_tags",
+  foreignKey: "product_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
+// Associate Tags to Products, so that when we query Product, we should see all the associated tags 
+// that the product has
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: "tagged_products",
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
