4f37dc558ce8577cf3e3b6941b7f94fce0884a7e
gigi.upNorth <leung.gerry@gmail.com>
2022-08-27 12:55:33 -0400

    Add product delete end point

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -134,6 +134,24 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbProductData) => {
+      if (!dbProductData) {
+        res.status(404).json({ message: "No Tag found with the ID" });
+        return;
+      }
+
+      // return sucess delete
+      res.json(dbProductData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
